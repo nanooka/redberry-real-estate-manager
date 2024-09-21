7f005ab3038fbe3acc24519107dc3a429874e203
nanooka <102428722+nanooka@users.noreply.github.com>
2024-09-21 23:54:07 +0400

    final

--- src/services/api/postData.js
+++ src/services/api/postData.js
@@ -1,5 +1,4 @@
-const token = "9cfe3731-3b40-4381-a6ac-3be97386a45c";
-// const token = "9d0eba29-1281-4c0d-b431-e0620399b660";
+const token = "9d0eba29-1281-4c0d-b431-e0620399b660";
 const URL = "https://api.real-estate-manager.redberryinternship.ge/api";
 
 export const addAgent = async (data) => {
